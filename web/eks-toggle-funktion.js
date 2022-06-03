3f76f3a34c3917fb6130613e586f01f296825a2c
cph-jn362 <71628258+cph-jn362@users.noreply.github.com>
2022-06-03 07:38:44 +0200

    Add files via upload

---
+++ web/eks-toggle-funktion.js
@@ -0,0 +1,8 @@
+function toggleFunktion2() {
+    var x = document.querySelector(".boks4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
---
+++ web/nav-toggle-funktion.js
@@ -0,0 +1,8 @@
+function toggleFunktion() {
+    var x = document.querySelector(".navbar-toggle");
+    if (x.style.display === "block") {
+      x.style.display = "none" ;
+    } else {
+      x.style.display = "block";
+    }
+  }
